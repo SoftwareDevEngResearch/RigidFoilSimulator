b7490242024aa255b0fe24f80930292f16c54252
Vickie Ngo <ngov@oregonstate.edu>
2020-05-28 15:56:48 -0700

    updating processRawWallShearData and merging inputs with motionProfile

--- ansysFiles/talkToAnsys.py
+++ ansysFiles/talkToAnsys.py
@@ -2,6 +2,20 @@ import os
 import subprocess
 import shutil
 
+
+fluent_path = shutil.which("fluent")
+
+WB_path=fluent_path[0:int(fluent_path.find("fluent"))] + r"Framework\bin\Win64\RunWB2.exe"
+
+subprocess.Popen([WB_path, '-R', 'testTemplate.wbjn', '-I'])
+
+
+
+
+
+
+
+
 # worDir = os.getcwd()
 
 
@@ -9,7 +23,14 @@ import shutil
 # # allCmds = "{}; {}".format(cmd1, cmd2) 
 # # https://stackoverflow.com/questions/9649355/python-how-to-run-multiple-commands-in-one-process-using-popen
 
-fluent_path = shutil.which("fluent")
-version_path=fluent_path[0:int(fluent_path.find("fluent"))] + r"Framework\bin\Win64\RunWB2.exe"
 
-subprocess.Popen([version_path, '-R', 'testTemplate.wbjn', '-I'])
+
+# This one does not, where the result should be:
+#   C:\Program Files\ANSYS Inc\ANSYS Student\v201\Framework\bin\Win64\RunWB2.exe
+RunWB2_path = shutil.which("RunWB2")
+
+# This is what I want to put the variable in:
+subprocess.Popen([PATH, '-R', 'testTemplate.wbjn', '-I'])
+
+
+
