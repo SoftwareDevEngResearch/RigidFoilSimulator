74f83791d99cabd723018faad75aa54bbbef2425
Vickie Ngo <63024851+vickie-ngo@users.noreply.github.com>
2020-05-29 10:18:40 -0700

    Updated WallShearProcess py (#4)
    * big mess
    
    * empty file
    
    * Update motionParam.py
    
    modifying for new pull request
    
    * adding ansys file
    
    * adding new acript for generating geometry in Ansys and for running Ansys Journal
    
    * adding ansys files
    
    * Added workbench project files
    
    * added data processing files
    
    * added if name (still broken)
    
    * adding data procesing files and fixing hard coded directories
    
    * updating processRawWallShearData and merging inputs with motionProfile
    
    * adding example data processing file for plotting
    
    * Add files via upload
    
    * fixed data processing and included wall shear eample data

--- ansysFiles/talkToAnsys.py
+++ ansysFiles/talkToAnsys.py
@@ -2,14 +2,15 @@ import os
 import subprocess
 import shutil
 
+fluent_path = shutil.which("fluent")
+
+WB_path=fluent_path[0:int(fluent_path.find("fluent"))] + r"Framework\bin\Win64\RunWB2.exe"
+
+subprocess.Popen([WB_path, '-R', 'testTemplate.wbjn', '-I'])
+
 # worDir = os.getcwd()
 
 
 # # allCmds = "{}; {}".format(cmd1 cmd2)
 # # allCmds = "{}; {}".format(cmd1, cmd2) 
 # # https://stackoverflow.com/questions/9649355/python-how-to-run-multiple-commands-in-one-process-using-popen
-
-fluent_path = shutil.which("fluent")
-version_path=fluent_path[0:int(fluent_path.find("fluent"))] + r"Framework\bin\Win64\RunWB2.exe"
-
-subprocess.Popen([version_path, '-R', 'testTemplate.wbjn', '-I'])
