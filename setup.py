1323140a2058ce4ff9eab2ba751ce7edad63252e
Vickie Ngo <63024851+vickie-ngo@users.noreply.github.com>
2020-06-10 16:08:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     package_data={'RigidFoilSimer': [
         'AnsysFiles/*',
         'Tests/*'
-        'Tests/Assets/*.txt','Tests/Assets/*.c'
+        'Tests/Assets/*',
         'AnsysFiles/WorkbenchProjectTemplate_files/*',
         'AnsysFiles/WorkbenchProjectTemplate_files/dp0/*',
         'AnsysFiles/WorkbenchProjectTemplate_files/dp0/FFF/DM/*',
